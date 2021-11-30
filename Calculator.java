f8ecd52012714563be4fdb7b241876c188eb8d5b
Aiman Kayad <Akaya005@fiu.edu>
2021-11-30 15:52:33 -0500

    Update Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -4,7 +4,9 @@ private int y; // private is just a best practice for class variables
 
 
 /* 
-A Java Method looks like this, most other langauges call them Functions. This is typically a bite-sized piece of code used in or Main class, also sometimes called the  "driver" or "tester" class.
+ A Java Method looks like this, most other langauges call them Functions. 
+ This is typically a bite-sized piece of code that we put to use in our Main class, 
+ also sometimes called the  "driver" or "tester" class.
 */
 public int add(int x, int y) 
 {
